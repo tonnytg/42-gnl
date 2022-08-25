f19f57898a192e2bc8470acd67f2f8e983748815
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2022-08-24 23:12:40 -0300

    fix: solve the problem of core
    Tests are done with core

--- srcs/get_next_line.c
+++ srcs/get_next_line.c
@@ -1,32 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antthoma <antthoma@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/16 01:41:27 by antthoma          #+#    #+#             */
+/*   Updated: 2022/08/25 04:11:32 by antthoma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_data(int fd, char *data)
+char	*ft_check_cache(char *cache, char *line, int fd, int *x)
 {
-	char buf[BUFFER_SIZE];
-	int bytes_read;
-	int bytes_transfer;
+	cache = ft_read_buff(fd);
+	*x = 0;
+	if (!cache)
+		if (line[0] == '\0')
+			return (NULL);
+	return (cache);
+}
 
-	bytes_transfer = BUFFER_SIZE;
+char	*run_loop(char *cache, char *line, int *x, int *trigger)
+{
+	int	length;
 
-	while ( (bytes_read = read(fd, buf, sizeof(buf) - 1)) > 0 )
+	length = ft_strlen(cache);
+	while (*x < length)
 	{
-		buf[bytes_read] = '\0';
-		data = ft_str_join(data, buf);
+		line = ft_char_append(line, cache[*x]);
+		if (cache[*x] == '\n')
+		{
+			*trigger = 1;
+			*x = *x + 1;
+			break ;
+		}		
+		*x = *x + 1;
 	}
-	return (data);
+	return (line);
+}
+
+char	*build_line(int fd)
+{
+	static char	*cache;
+	char		*line;
+	static int	x;
+	int			trigger;
+
+	line = ft_strdup("");
+	trigger = 0;
+	while (1)
+	{	
+		if (!cache)
+		{
+			cache = ft_check_cache(cache, line, fd, &x);
+		}
+		if (!cache)
+			return (line);
+		line = run_loop(cache, line, &x, &trigger);
+		if (trigger == 1)
+			break ;
+		free(cache);
+		cache = NULL;
+	}
+	return (line);
 }
 
 char	*get_next_line(int fd)
 {
-	static char *data;
-	static int start;
+	char	*line;
 
-	if (!data)
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	line = build_line(fd);
+	if (line[0] == '\0')
 	{
-		data = "";
-		start = 0;
+		free(line);
+		return (NULL);
 	}
-	data = get_data(fd, data);
-	return (ft_split_str(data, &start));
+	return (line);
 }
