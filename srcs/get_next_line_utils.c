634358854cc5f73c1e74f26c892ce935731fa24a
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2022-08-24 23:13:49 -0300

    fix: update header file
    Add list functions I needed

--- srcs/get_next_line_utils.c
+++ srcs/get_next_line_utils.c
@@ -1,88 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: antthoma <antthoma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/16 01:41:41 by antthoma          #+#    #+#             */
-/*   Updated: 2022/08/24 13:59:26 by antthoma         ###   ########.fr       */
+/*   Created: 2022/08/16 01:41:34 by antthoma          #+#    #+#             */
+/*   Updated: 2022/08/24 13:54:07 by antthoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-size_t	ft_strlen(char const *str)
-{
-	size_t	i;
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
 
-	i = 0;
-	while (!str || str[i] != '\0')
-		i++;
-	return (i);
-}
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
 
-char	*ft_read_buff(int fd)
-{
-	char	*buff;
-	char	*slice;
-	int		*bytes_transfer;
-	int		bytes_read;
+size_t	ft_strlen(char const *str);
+char	*ft_strdup(char *str);
+char	*ft_char_append(char *s1, char c);
+char	*get_next_line(int fd);
+char	*ft_read_buff(int fd);
 
-	if (BUFFER_SIZE < 0)
-		return (NULL);
-	bytes_transfer = malloc(BUFFER_SIZE * sizeof(int *));
-	*bytes_transfer = BUFFER_SIZE;
-	buff = malloc(BUFFER_SIZE * sizeof(char *) + 1);
-	if (!buff)
-		return (NULL);
-	bytes_read = read(fd, buff, *bytes_transfer);
-	free(bytes_transfer);
-	if (bytes_read < 1)
-	{
-		free(buff);
-		return (NULL);
-	}
-	buff[bytes_read] = '\0';
-	slice = ft_strdup(buff);
-	free(buff);
-	return (slice);
-}
-
-char	*ft_strdup(char *str)
-{
-	char	*dest;
-	size_t	i;
-
-	dest = (char *) malloc((ft_strlen(str) + 1) * sizeof(char));
-	if (!dest)
-		return (NULL);
-	i = 0;
-	while (*str)
-		dest[i++] = *str++;
-	dest[i] = '\0';
-	return (dest);
-}
-
-char	*ft_char_append(char *s1, char c)
-{
-	int		i;
-	char	*m;
-
-	if (!s1 || !c)
-		return (NULL);
-	m = malloc((ft_strlen(s1) + sizeof(c)) + 1);
-	if (!m)
-		return (NULL);
-	i = 0;
-	while (s1[i] != '\0')
-	{
-		m[i] = s1[i];
-		i++;
-	}
-	m[i] = c;
-	i++;
-	m[i] = '\0';
-	free(s1);
-	return (m);
-}
+#endif
