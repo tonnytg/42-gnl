ca5c12a35d46f737d6a2976d4d87dd5fd4f939c6
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-08-17 11:10:50 +0000

    fix: gnl headers

--- srcs/get_next_line.h
+++ srcs/get_next_line.h
@@ -5,12 +5,14 @@
 # include <unistd.h>
 
 char	*get_next_line(int fd);
-
-// Helper functions
 int		ft_count_word(char *str, int *start);
-int		ft_strlen(char const *str);
 char	*ft_str_join(char const *s1, char const *s2);
 char	*ft_split_str(char *str, int *start);
 char	*get_data(int fd, char *data);
+char    *ft_read_buff(int fd);
+char    *ft_check_cache(char *cache, char *line, int fd, int *x);
+char    *ft_strdup(char *str);
+size_t  ft_strlen(char const *str);
+char    *ft_char_append(char *s1, char c);
 
 #endif
